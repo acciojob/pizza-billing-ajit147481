60e62f3551450f6afc4f417325a033b6248f5089
ajit147481 <rautajit1234@gmail.com>
2022-12-26 23:17:35 +0530

    a

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,16 +9,19 @@ public class Pizza {
     private boolean extra_toppings=false;
     private boolean add_takeAway=false;
     private boolean is_bill_generated=false;
-    int chees_price;
-    int topping_price;
+    private int chees_price;
+    private int topping_price;
 
     public Pizza(Boolean isVeg){
         // your code goes here
         this.isVeg = isVeg;
+        this.chees_price=80;
         if(isVeg==true){
             this.price=300;
+            this.topping_price=70;
         }else{
             this.price=400;
+            this.topping_price=120;
         }
     }
 
@@ -30,28 +33,23 @@ public class Pizza {
     public void addExtraCheese(){
         // your code goes here
         if(extra_cheez==false){
-            this.price+=80;
-            extra_cheez=true;
+            this.price+=this.chees_price;
+            this.extra_cheez=true;
         }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(extra_toppings==false && isVeg==true){
-            extra_toppings=true;
-            this.price+=70;
-        }else
-        if(!extra_toppings && !isVeg){
-            extra_toppings=true;
-            topping_price=120;
-            this.price+=topping_price;
+        if(extra_toppings==false){
+            this.extra_toppings=true;
+            this.price+=this.topping_price;
         }
     }
 
     public void addTakeaway(){
         // your code goes here
         if(add_takeAway==false){
-            add_takeAway=true;
+            this.add_takeAway=true;
             this.price+=20;
         }
     }
@@ -61,10 +59,10 @@ public class Pizza {
         if(is_bill_generated==false) {
             this.bill = "Base Price Of The Pizza: "+this.price + "\n";
             if (extra_cheez == true) {
-                bill += "Extra Cheese Added: "+80 + "\n";
+                bill += "Extra Cheese Added: "+chees_price + "\n";
             }
             if (extra_toppings = true && isVeg) {
-                bill += "Extra Toppings Added: "+70 + "\n";
+                bill += "Extra Toppings Added: "+topping_price + "\n";
             }
             if (add_takeAway == true) {
                 bill += "Paperbag Added: "+20 + "\n";
